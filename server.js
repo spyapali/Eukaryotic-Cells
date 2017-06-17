baa017a3b0a683349c5bc55f60d0e6d5b2d725ad
Shalini Pyapali <shalu.pyapali@gmail.com>
2017-06-17 15:26:51 -0700

    Add node modules.

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by spyapali on 6/17/17.
+ */
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
+
+
